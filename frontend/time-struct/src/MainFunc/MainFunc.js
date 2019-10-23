8c3efa4694a425d472aa453546b704843cb6277d
Gurvir Singh <charizardzx9@gmail.com>
2019-10-23 08:54:29 -0400

    Added framwework for several

--- frontend/time-struct/src/MainFunc/MainFunc.js
+++ frontend/time-struct/src/MainFunc/MainFunc.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 
-const MainFunc = () => {
+const MainFunc = (props) => {
+  if (props.index === 1){
   return(
   <div className='MainFunc'>
-    <>
+    <p>Calender</p>
   </div>
   );
+  }
+  if (props.index === 2){
+    return(
+    <p>Tasks</p>
+    );
+  }
+
 }
 
 export default MainFunc;
