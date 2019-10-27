859ae279d20c975492bff4cbda712db8275dff68
Paul N <niziolek.paul@gmail.com>
2019-10-27 11:12:04 -0400

    moving stuff around

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,15 +7,15 @@ import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhances(applyMiddleware(thunk)));
+
 const app = (
   <Provider store={store}>
     <App />
   </Provider>
 );
-
-const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhances(applyMiddleware(thunk)));
 
 ReactDOM.render(app, document.getElementById("root"));
 serviceWorker.unregister();
