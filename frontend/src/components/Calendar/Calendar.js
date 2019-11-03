18da9a20aa750397122d0dfcff176c2f5ccdc35b
Gurvir <charizardzx9@gmail.com>
2019-11-03 10:49:11 -0500

    I cant be bothered to write a good commit message

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -1,52 +1,88 @@
-import React, {useState} from 'react'
+import React, {Component} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' 
 import interactionPlugin from '@fullcalendar/interaction' 
 import getTodaysDate from '../../Today.js'
-//import state from '../../App.js'
-//import getTodaysDate from '../../Today.js'
+import Axios from 'axios';
 
-const Calendar = (props) => {
-    const [ EventDayState, SetEventDayState ] = useState(
-        {
-            eventDay: getTodaysDate()
-        }
-    );
+class Calendar extends Component {
+
+    state = {
+        eventDay: getTodaysDate(),
+        events: [],
+        eventName: ""
+    };
   
-    const dateClickHandler = (arg) => {
+    dateClickHandler = (arg) => {
 
-        SetEventDayState(
+        this.setState(
             {
-                eventDay: getTodaysDate()
+                eventDay: arg.dateStr
             }
         );
 
     }
 
-    return(
-        <div className='CalendarWidget'>
-            <h3>Click a date and add and event:</h3>
-            <p>Date selected: {EventDayState}</p>
-            <form>
-                <input type='text' placeholder='Event Name'></input>
-                <br></br>
-                <input type='text' placeholder='Description'></input>
-                <br></br>
-                <button>Add event</button>
-            </form>
-            <FullCalendar 
-                defaultView="dayGridMonth"
-                plugins={[dayGridPlugin, interactionPlugin]}
-                events={[
-                { title: 'Congressional App Challenge Due', date: '2019-11-01' }
-                ]}
-                dateClick={dateClickHandler}
-                
-            />
-  
-        </div>
-    );
+    addEventHandler = () => {
+        let EventToAdd = {
+            eventName: this.state.eventName,
+            eventDesc: "Desc",
+            eventDate: this.state.eventDay
+          }
+          
+        console.log(EventToAdd)
+        Axios
+            .post("http://127.0.0.1:8000/api/events/", EventToAdd)
+              .then(response => {
+                console.log(response);
+              })
+              .catch(error => {
+                console.log(error);
+        });
+    }
+
+    inputHandler = (event) => {
+        this.setState(
+            {
+                eventName: event.target.value
+            }
+        );
+    }
     
+    componentDidMount() {
+        Axios.get("http://127.0.0.1:8000/api/events/")
+    .then(response => {
+      console.log(response)
+      this.setState(
+        {
+          events: response
+        }
+      )
+    });
+    }
+
+    render(){
+        return(
+            <div className='CalendarWidget'>
+                <h3>Click a date and add and event:</h3>
+                <p>Date selected: {this.state.eventDay}</p>
+                <form>
+                    <input onChange={this.inputHandler}  type='text' placeholder='Event Name'></input>
+                </form>
+                <button onClick={this.addEventHandler}>Add Event</button>
+                <FullCalendar 
+                    defaultView="dayGridMonth"
+                    plugins={[dayGridPlugin, interactionPlugin]}
+                    events={[
+                    { title: 'Congressional App Challenge Due', date: '2019-11-01' }
+                    ]}
+                    dateClick={this.dateClickHandler}
+                    selectable="false"
+                />
+    
+            </div>
+        );
+    }
 };
 
 export default Calendar;
